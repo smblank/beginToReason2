5d56ec80f42e382f0ed7cd7bacda4b6455362b8e
rhit-hurtigna <73034845+rhit-hurtigna@users.noreply.github.com>
2021-04-03 16:50:56 -0400

    Exporting quantifiable relation data to CSV

--- data_analysis/py_helper_functions/connectivity_analysis/matrix_maker.py
+++ data_analysis/py_helper_functions/connectivity_analysis/matrix_maker.py
@@ -1,5 +1,5 @@
 import math
-
+import csv
 from core.models import LessonSet
 from data_analysis.models import DataLog
 from data_analysis.py_helper_functions.datalog_helper import locate_confirms
@@ -12,7 +12,7 @@ def lesson_to_matrix(set_id, lesson_index, attenuation_constant):
     query = DataLog.objects.filter(lesson_key_id=lesson_id).order_by('user_key', 'time_stamp')
     if not query:
         return
-    matrix, answer_to_index = _create_matrix(query)
+    matrix, answer_to_index, answer_count = _create_matrix(query)
     prev_student = query[0].user_key
     chain = []
     for log in query:
@@ -24,8 +24,9 @@ def lesson_to_matrix(set_id, lesson_index, attenuation_constant):
         chain.append(answer_to_index.get(log.code))
     # Post iteration, gotta do this for the last student
     _disassemble_chain(matrix, chain, attenuation_constant)
-    _display_matrix(matrix, answer_to_index)
-    _display_matrix(_relations_matrix(matrix), answer_to_index)
+    _write_to_csv(matrix, _relations_matrix(matrix), answer_to_index, "matrix_results_rads_pi", answer_count)
+    # _display_matrix(matrix, answer_to_index)
+    # _display_matrix(_relations_matrix(matrix), answer_to_index)
 
 
 # returns a matrix and hashmap between unique answers and indices in the matrix,
@@ -39,7 +40,7 @@ def _create_matrix(query):
     matrix = []
     for index in range(answers_found):
         matrix.append(_make_row(answers_found))
-    return matrix, answer_to_index
+    return matrix, answer_to_index, answers_found
 
 
 # To add to the dict in a way that handles whitespace inconsistency. Returns what to increment answers_found by
@@ -89,10 +90,10 @@ def _relations_matrix(matrix):
         relation_matrix.append(_make_row(side_length))
 
     for row_index, row in enumerate(matrix):
-        for other_row_index in range(row_index + 1, side_length):
-            angle = _find_angle(row, matrix[other_row_index])
-            relation_matrix[row_index][other_row_index] = angle
-            relation_matrix[other_row_index][row_index] = angle  # Symmetric because it's relational
+        for other_row_index in range(row_index, side_length):
+            relation_strength = (math.pi / 2 - _find_angle(row, matrix[other_row_index])) / (math.pi / 2)
+            relation_matrix[row_index][other_row_index] = relation_strength
+            relation_matrix[other_row_index][row_index] = relation_strength  # Symmetric because it's relational
 
     return relation_matrix
 
@@ -108,8 +109,8 @@ def _display_matrix(matrix, answer_to_index):
 def _find_angle(row1, row2):
     magnitude_product = _magnitude(row1) * _magnitude(row2)
     if magnitude_product == 0:
-        return "NaN"
-    return (math.pi / 2 - math.acos(_dot_product(row1, row2) / magnitude_product)) / (math.pi / 2)
+        return float("NaN")
+    return math.acos(_dot_product(row1, row2) / magnitude_product)
 
 
 # Basic dot product
@@ -126,3 +127,36 @@ def _magnitude(row):
     for value in row:
         quadrature_sum += math.pow(value, 2)
     return math.sqrt(quadrature_sum)
+
+
+def _write_to_csv(prediction_matrix, relation_matrix, string_to_index, file_name, dimension):
+    answers_list = _reverse_dict(string_to_index, dimension)
+    prediction_matrix = _add_headers(prediction_matrix, answers_list, "TO\\FROM")
+    relation_matrix = _add_headers(relation_matrix, answers_list, "")
+    csv_file = open(file_name + ".csv", 'w', newline='')
+    writer = csv.writer(csv_file)
+    writer.writerow(["Predictions"])
+    writer.writerows(prediction_matrix)
+    writer.writerows([[], ["Relations"]])
+    writer.writerows(relation_matrix)
+    csv_file.close()
+
+
+def _reverse_dict(dictionary, list_length):
+    index_to_ans = [""] * list_length
+    for pair in dictionary.items():
+        if not index_to_ans[pair[1]]:
+            index_to_ans[pair[1]] = pair[0]
+    return index_to_ans
+
+
+def _add_headers(matrix, headers, top_left):
+
+    # Add on left
+    for index, row in enumerate(matrix):
+        matrix[index] = [headers[index]] + row
+
+    # Add on top
+    matrix = [[top_left] + headers] + matrix
+
+    return matrix
