6433360cc722bb2e1259458da861d448f3cbf10a
rhit-hurtigna <73034845+rhit-hurtigna@users.noreply.github.com>
2021-04-03 13:48:18 -0400

    Calculating angles between vectors in Katz matrix

--- data_analysis/py_helper_functions/connectivity_analysis/matrix_maker.py
+++ data_analysis/py_helper_functions/connectivity_analysis/matrix_maker.py
@@ -1,3 +1,5 @@
+import math
+
 from core.models import LessonSet
 from data_analysis.models import DataLog
 from data_analysis.py_helper_functions.datalog_helper import locate_confirms
@@ -23,6 +25,7 @@ def lesson_to_matrix(set_id, lesson_index, attenuation_constant):
     # Post iteration, gotta do this for the last student
     _disassemble_chain(matrix, chain, attenuation_constant)
     _display_matrix(matrix, answer_to_index)
+    _relations_matrix(matrix)
 
 
 # returns a matrix and hashmap between unique answers and indices in the matrix,
@@ -78,8 +81,49 @@ def _create_connections(matrix, chain, attenuation_constant, chain_index, matrix
         strength *= attenuation_constant  # Recursively weakens by the constant
 
 
+def _relations_matrix(matrix):
+    # Make matrix
+    side_length = len(matrix)
+    relation_matrix = []
+    for index in range(side_length):
+        relation_matrix.append(_make_row(side_length))
+
+    for row_index, row in enumerate(matrix):
+        for other_row_index in range(row_index + 1, side_length):
+            angle = _find_angle(row, matrix[other_row_index])
+            relation_matrix[row_index][other_row_index] = angle
+            relation_matrix[other_row_index][row_index] = angle  # Symmetric because it's relational
+
+    _display_matrix(relation_matrix, "Angles:")
+
+
 # Displays the matrix with newlines and all that good stuff
 def _display_matrix(matrix, answer_to_index):
     print(answer_to_index)
     for row in matrix:
         print(row)
+
+
+# Using dot product (law of cosines)
+def _find_angle(row1, row2):
+    magnitude_product = _magnitude(row1) * _magnitude(row2)
+    if magnitude_product == 0:
+        return "NaN"
+    print(_dot_product(row1, row2) / magnitude_product)
+    return math.acos(_dot_product(row1, row2) / magnitude_product)
+
+
+# Basic dot product
+def _dot_product(row1, row2):
+    ans = 0
+    for index, value in enumerate(row1):
+        ans += value * row2[index]
+    return ans
+
+
+# Magnitude of vector
+def _magnitude(row):
+    quadrature_sum = 0
+    for value in row:
+        quadrature_sum += math.pow(value, 2)
+    return math.sqrt(quadrature_sum)
